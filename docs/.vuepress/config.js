dfbbf9d012e8e7ac862f70cf35df617f34e537e1
zhoumaoling <zhoumaoling@baidu.com>
2024-08-01 15:59:20 +0800

    站点部署完成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,14 @@
 module.exports = {
+  title: 'Mi Components',
+  // 站点配置在子目录mi-components下面。注意这里的/必须有，否则静态资源路径不对
+  base: '/mi-components/',
   themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Components', link: '/componentsDocs/card' },
+      { text: 'Github', link: 'https://mynightwish.github.io/mi-components/'},
+      { text: 'NPM', link: 'https://www.npmjs.com/package/@mynightwish/mi-components'}
+    ],
     sidebar: [
       '/',
       '/componentsDocs/card'
